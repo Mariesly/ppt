ab4ff15b1da1ab75b3a1b9e80a095650b808f202
Mariesly <marieslyaguilar@gmail.com>
2023-02-17 11:47:54 -0400

    curso basico de javascript, empezando a realizar el juego de piedra papel o tijera

---
+++ primerosapuntes.js
@@ -0,0 +1,43 @@
+//Tipos de valores//
+
+//Valores primitivos//
+//numerico: se escribe normla en numeros//
+64
+
+//cadena de texto: se escribe entre comillas//
+"Mariesly Aguilar"
+
+//Booleano//
+true
+false
+
+//indefinido//
+null
+undefined
+
+//valores no primitivos u orientado a objetos//
+//array//
+[1,2,3]
+//Valor tipo objeto//
+{nombre:"Diego"}
+
+
+//Variables//
+var nombre = "Diego"
+var elemento = ["hola", "adios"];
+var persona ={
+    nombre: "Diego",
+    edad: 30
+}
+
+//Declaracion de funciones//
+
+//funcion declarativa//
+function mifuncion(){
+    return 3;
+}
+
+//Funcion expresiva
+var mifuncion= function(){
+    return a + b;
+}
