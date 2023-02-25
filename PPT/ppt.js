de6b5c9f423b098297d4f20352eabd99afcd2495
Mariesly <marieslyaguilar@gmail.com>
2023-02-24 20:28:12 -0400

    juego funcionando

--- PPT/ppt.js
+++ PPT/ppt.js
@@ -1,23 +1,46 @@
-var persona = document.querySelector('input');
-var resultado
-
-
-
-
+var resultado = null
 
 const nombre = ["piedra", "papel", "tijera"];
-var maquina = nombre[Math.floor(Math.random() * nombre.length)];
-console.log("La maquina escogio: "+ maquina);
+let persona = nombre[0]
 
-if (((persona=="piedra")&&(maquina=="tijera")) || ((persona=="papel")&&(maquina=="piedra")) || ((persona=="tijera")&&(maquina=="papel"))) {
-    resultado="Felicidades! Ganaste"
-    console.log(resultado)
-}
-else if(persona==maquina){
-    resultado="empatados"
-    console.log(resultado)
-}
-else{
-    resultado="Lo siento, perdiste"
-    console.log(resultado)
-}
+const $PPTSelect = document.querySelector("#PPT")
+
+$PPTSelect.addEventListener("click", (e) => {
+    persona = nombre[parseInt(e.target.value)]
+    console.log(persona)
+})
+
+const $inputSubmit = document.querySelector("#input-submit")
+
+const $resultText = document.querySelector("#result")
+const $machineSelection = document.querySelector("#machineSelection")
+
+$inputSubmit.addEventListener("click", (e) => {
+    e.preventDefault()
+ 
+    var maquina = nombre[Math.floor(Math.random() * nombre.length)];
+    $machineSelection.textContent = "La maquina escogio: " + maquina;
+
+    console.log({ maquina, persona })
+
+    if (((persona == "piedra") && (maquina == "tijera")) || ((persona == "papel") && (maquina == "piedra")) || ((persona == "tijera") && (maquina == "papel"))) {
+        resultado = "Felicidades! Ganaste"
+        console.log(resultado)
+    }
+    else if (persona == maquina) {
+        resultado = "empatados"
+        console.log(resultado)
+    }
+    else {
+        resultado = "Lo siento, perdiste"
+        console.log(resultado)
+    }
+
+    $resultText.textContent = resultado
+
+})
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    console.log("hola mary")
+
+})
